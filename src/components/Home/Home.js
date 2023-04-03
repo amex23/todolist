a60a8809efc659a5a102ab3a1a8ef0b01a5aa444
amex23 <randelb2022@gmail.com>
2023-04-04 00:05:21 +0800

    new update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -97,6 +97,22 @@ export default class Home extends Component {
     });
   };
 
+
+  addComment = () => {
+    let {comment_array, comment} = this.state;
+    let obj = {
+      id: comment_array.length == 0 ? 1 : comment_array[comment_array.length - 1].id + 1,
+      name: comment,
+      is_editing: false,
+      is_done: false,
+    };
+    comment_array.push (obj);
+    this.setState ({
+      comment_array: comment_array,
+      comment: '',
+    });
+  };
+
   render () {
     return (
       <div className='bg-red-400 w-full flex justify-start flex-col items-center h-screen gap-10'>
@@ -125,87 +141,102 @@ export default class Home extends Component {
         </div>
 
         {this.state.todo_array.length > 0
-          ? <div>
-              <table className="centerTable" style={{marginTop: 20}}>
-                <thead>
-                  <tr>
-                    <th>Task</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
+          ? <div className='bg-pink-400 w-9/12 flex justify-center items-center py-5 px-5 flex-col gap-3'>
+              <div className="bg-blue-200 w-full">
+              
+                    <div className='flex'>
+                      <div className='w-1/2 p-4'>Taskzzz</div>
+                      <div className='w-1/2 p-4'>Action</div>
+                    </div>
+                  
                 {this.state.todo_array.map ((object, i) => {
                   return (
-                    <tbody>
-                      <tr>
-                        <td>
-                          {object.is_editing
-                            ? <Textarea
-                                id="standard-basic"
-                                value={this.state.edit_task}
-                                onChange={e => this.editTask (e.target.value)}
-                              />
-                            : object.is_done
-                                ? <s>{object.name}</s>
-                                : <span>{object.name}</span>}
-                        </td>
-                        <td>
-                          {object.is_editing
-                            ? <div>
-                                <Button
-                                  className="button_style"
-                                  variant="outlined"
-                                  color="primary"
-                                  size="small"
-                                  disabled={this.state.edit_task == ''}
-                                  onClick={e => this.saveEditTask (object)}
-                                >
-                                  Save
-                                </Button>
-                                <Button
-                                  className="button_style"
-                                  variant="outlined"
-                                  color=""
-                                  size="small"
-                                  onClick={e => this.edit (object)}
-                                >
-                                  Cancel
-                                </Button>
-                              </div>
-                            : <div>
-                                <Button
-                                  className="button_style"
-                                  variant="outlined"
-                                  color="primary"
-                                  size="small"
-                                  onClick={e => this.edit (object)}
-                                >
-                                  Edit
-                                </Button>
-                                <Button
-                                  className="button_style"
-                                  variant="outlined"
-                                  color="secondary"
-                                  size="small"
-                                  disabled={object.is_done}
-                                  onClick={e => this.done (object)}
-                                >
-                                  Done
-                                </Button>
-                                <Button
-                                  className="button_style"
-                                  variant="outlined"
-                                  size="small"
-                                  onClick={e => this.delete (object)}
-                                >
-                                  Delete
-                                </Button>
-                              </div>}
-                        </td>
-                      </tr>
-                    </tbody>
+                    <div className='bg-violet-400 w-full' >
+                        <div className='w-full bg-green-400 flex py-4'>
+                          <div className='w-1/2 bg-violet-100 py-4 px-4'>
+                            {object.is_editing
+                              ? <Textarea
+                                  id="standard-basic"
+                                  value={this.state.edit_task}
+                                  onChange={e => this.editTask (e.target.value)}
+                                />
+                              : object.is_done
+                                  ? <s>{object.name}</s>
+                                  : <span>{object.name}</span>}
+                          </div>
+                          <div className='w-1/2 bg-red-200 gap-3 py-4 px-4'>
+                            {object.is_editing
+                              ? <div className='flex gap-3'>
+                                  <Button
+                                    className="button_style"
+                                    variant="outlined"
+                                    color="primary"
+                                    size="small"
+                                    disabled={this.state.edit_task == ''}
+                                    onClick={e => this.saveEditTask (object)}
+                                  >
+                                    Save
+                                  </Button>
+                                  <Button
+                                    className="button_style"
+                                    variant="outlined"
+                                    color=""
+                                    size="small"
+                                    onClick={e => this.edit (object)}
+                                  >
+                                    Cancel
+                                  </Button>
+                                </div>
+                              : <div className="flex gap-3"> 
+                                  <Button
+                                    className="button_style"
+                                    variant="outlined"
+                                    color="primary"
+                                    size="small"
+                                    onClick={e => this.edit (object)}
+                                  >
+                                    Edit
+                                  </Button>
+                                  <Button
+                                    className="button_style"
+                                    variant="outlined"
+                                    color="secondary"
+                                    size="small"
+                                    disabled={object.is_done}
+                                    onClick={e => this.done (object)}
+                                  >
+                                    Done
+                                  </Button>
+                                  <Button
+                                    className="button_style"
+                                    variant="outlined"
+                                    size="small"
+                                    onClick={e => this.delete (object)}
+                                  >
+                                    Delete
+                                  </Button>
+                                </div>}
+                          </div>   
+                        </div>
+                        <div className="w-full bg-yellow-200 py-4 px-4">
+                          <p>comment</p>
+
+                          <Button
+                            className="bg-yellow-400"
+                            variant="contained"
+                            color="primary"
+                            size="small"
+                            disabled={this.state.comment == ''}
+                            onClick={this.addComment}
+                          >
+                            Add
+                          </Button>
+                        </div>
+                    </div>                    
                   );
                 })}
-              </table>
+              </div>
+                
             </div>
           : <h2>Nothing to do!</h2>}
       </div>
